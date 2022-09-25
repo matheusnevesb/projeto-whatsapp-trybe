8c876279ace67b1fc0fef63c463d7e0a7fbcc4e8
matheus <matheusnbbg@hotmail.com>
2022-09-25 00:09:50 -0300

    centralizado conteudo e adicionado funcionalidade de ir ate acessibilidade

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 let mais = document.querySelector('#mais')
+let acessibilidade = document.querySelector('#acessibilidade')
+let voltar = document.querySelector('#voltar')
 
 
 
@@ -10,4 +12,21 @@ mais.onclick = (e) => {
     }else{
         lista.classList.remove('esconder')
     }
+}
+
+acessibilidade.onclick = (e) => {
+    header = e.target.parentElement.parentElement.parentElement.parentElement
+    header.classList.add('esconder')
+    corpo = header.nextElementSibling
+    corpo.classList.remove('esconder')
+    console.log(header.nextElementSibling)
+
+}
+
+voltar.onclick = (e) => {
+    header = e.target.parentElement.offsetParent.children[0]
+    header.classList.remove('esconder')
+    corpo.classList.add('esconder')
+    lista.classList.add('esconder')
+
 }
