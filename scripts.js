38f6f25c96d2cf593dbf9a7850aeedb6653f8c3e
Douglas Santos <ds4208151@gmail.com>
2022-09-26 12:58:47 -0300

    algumas alteraçoes realizadas

--- scripts.js
+++ scripts.js
@@ -3,16 +3,11 @@ let acessibilidade = document.querySelector('#acessibilidade')
 let voltar = document.querySelector('#voltar')
 
 
-
 mais.onclick = (e) => {
-    lista = e.target.parentElement.nextElementSibling
-    aparece = lista.classList.contains('esconder')
-    if (!aparece){
-        lista.classList.add('esconder')
-    }else{
-        lista.classList.remove('esconder')
-    }
+    let lista = document.getElementById("list");
+    lista.classList.toggle('esconder')
 }
+
 
 acessibilidade.onclick = (e) => {
     header = e.target.parentElement.parentElement.parentElement.parentElement
